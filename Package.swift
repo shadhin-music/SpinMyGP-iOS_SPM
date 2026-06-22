15ced3d5f532303b97b7936756b799f64595e3db
Abdul Motalab Rakib <motalab@cloud7bd.com>
2026-06-22 13:44:54 +0600

    Package dependency added

--- Package.swift
+++ Package.swift
@@ -1,4 +1,5 @@
-// swift-tools-version:5.9
+// swift-tools-version: 5.9
+
 import PackageDescription
 
 let package = Package(
@@ -12,11 +13,24 @@ let package = Package(
             targets: ["SpinMyGP"]
         )
     ],
+    dependencies: [
+        .package(url: "https://github.com/SnapKit/SnapKit.git", from: "5.7.1"),
+        .package(url: "https://github.com/airbnb/lottie-ios.git", from: "4.6.1")
+    ],
     targets: [
         .binaryTarget(
-            name: "SpinMyGP",
-            url: "https://github.com/shadhin-music/SpinMyGP-iOS_SPM/releases/download/0.0.1/SpinMyGP.xcframework.zip",
+            name: "SpinMyGPBinary",
+            url: "https://github.com/shadhin-music/SpinMyGP-iOS_SPM/releases/download/0.0.2/SpinMyGP.xcframework.zip",
             checksum: "37f20c1f31916506a8c8173b65a6361244de416d35d960717f59cbdd9c6e5eab"
+        ),
+
+        .target(
+            name: "SpinMyGP",
+            dependencies: [
+                "SpinMyGPBinary",
+                .product(name: "SnapKit", package: "SnapKit"),
+                .product(name: "Lottie", package: "lottie-ios")
+            ]
         )
     ]
 )
