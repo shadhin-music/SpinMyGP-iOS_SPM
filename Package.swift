77a798f38dd98e0e8de0fa646673a0142445cddc
Abdul Motalab Rakib <motalab@cloud7bd.com>
2026-06-22 14:18:42 +0600

    package updated

--- Package.swift
+++ Package.swift
@@ -26,7 +26,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SpinMyGPBinary",
-            url: "https://github.com/shadhin-music/SpinMyGP-iOS_SPM/releases/download/0.0.2/SpinMyGP.xcframework.zip",
+            url: "https://github.com/shadhin-music/SpinMyGP-iOS_SPM/releases/download/0.0.3/SpinMyGP.xcframework.zip",
             checksum: "37f20c1f31916506a8c8173b65a6361244de416d35d960717f59cbdd9c6e5eab"
         ),
 
