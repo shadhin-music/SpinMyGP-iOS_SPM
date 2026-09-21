f059c149ba55c1d3d048e99f2705194c6d5822d3
Abdul Motalab Rakib <motalab@cloud7bd.com>
2026-09-21 17:21:40 +0600

    Fix binary target URL

--- Package.swift
+++ Package.swift
@@ -1,23 +1,27 @@
 // swift-tools-version: 5.9
+
 import PackageDescription
 
 let package = Package(
     name: "SpinMyGP",
+
     platforms: [
-        .iOS(.v13),
+        .iOS(.v15),
         .macOS(.v10_15)
     ],
+
     products: [
         .library(
             name: "SpinMyGP",
             targets: ["SpinMyGP"]
         )
     ],
+
     targets: [
         .binaryTarget(
-            name: "SpinMyGP",   // must match module name inside XCFramework
-            url: "https://github.com/shadhin-music/SpinMyGP-iOS_SPM/releases/download/0.1.6/SpinMyGP.xcframework.zip",
-            checksum: "74258b30b05eebda2e876a69f4dbb9db8c070e1a2560b52a5b1dec6663a3efa6"
+            name: "SpinMyGP",
+            url: "https://github.com/shadhin-music/SpinMyGP-iOS_SPM/releases/download/0.1.8/SpinMyGP.xcframework.zip",
+            checksum: "eee3a36062289e0b3e9062f5f907087644858b77b0a42ffb9c0f6e9b0e29eff6"
         )
     ]
 )
