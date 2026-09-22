e49e25cbf4d7e6ba77079146fc9eed16d7a27963
Abdul Motalab Rakib <motalab@cloud7bd.com>
2026-09-22 11:45:20 +0600

    Package dependencies added

--- Package.swift
+++ Package.swift
@@ -11,17 +11,39 @@ let package = Package(
     ],
 
     products: [
-        .library(
-            name: "SpinMyGP",
-            targets: ["SpinMyGP"]
-        )
-    ],
+            .library(
+                name: "SpinMyGP",
+                targets: ["SpinMyGPWrapper"]
+            )
+        ],
 
-    targets: [
-        .binaryTarget(
-            name: "SpinMyGP",
-            url: "https://github.com/shadhin-music/SpinMyGP-iOS_SPM/releases/download/0.1.8/SpinMyGP.xcframework.zip",
-            checksum: "eee3a36062289e0b3e9062f5f907087644858b77b0a42ffb9c0f6e9b0e29eff6"
+        dependencies: [
+            .package(
+                url: "https://github.com/SnapKit/SnapKit.git",
+                from: "6.0.0"
+            ),
+            .package(
+                url: "https://github.com/airbnb/lottie-ios.git",
+                from: "4.6.1"
+            )
+        ],
+
+        targets: [
+            .target(
+                name: "SpinMyGPWrapper",
+                dependencies: [
+                    "SpinMyGP",
+                    .product(name: "SnapKit", package: "SnapKit"),
+                    .product(name: "Lottie", package: "lottie-ios")
+                ],
+                path: "Sources/SpinMyGP"
+            ),
+            
+                .binaryTarget(
+                    name: "SpinMyGP",
+                    url: "https://github.com/shadhin-music/SpinMyGP-iOS_SPM/releases/download/0.1.9/SpinMyGP.xcframework.zip",
+                    checksum: "eee3a36062289e0b3e9062f5f907087644858b77b0a42ffb9c0f6e9b0e29eff6"
+                )
+            ]
         )
-    ]
-)
+
